c4cd3378a94c4546a099c3b2d59ced3ae35ceb88
beniTrainor <beni.trainor.96@gmail.com>
2022-03-13 18:15:11 +0100

    feat(slides-generator): use a many-to-many mapping between titles and images

--- ms-mr-language-dynamic-slide-generator/slide-generator.js
+++ ms-mr-language-dynamic-slide-generator/slide-generator.js
@@ -43,7 +43,7 @@ function random_color() {
 // =============================================================================
 
 
-// Slides information
+// Slide information
 // =============================================================================
 const slides_many_to_many = [
     {
@@ -51,65 +51,66 @@ const slides_many_to_many = [
             "Don't have dreams. Have goals.",
             "Learn and Enjoy with Miss & Mister Language",
             "Consigue tus objetivos en 2 meses con nuestro pack de 35 horas!",
-            "Necesitas ayuda para preparte con tu exámen oficial? Te ayudamos!"
+            "Necesitas ayuda para preparar tu exámen oficial? Te ayudamos!"
         ],
         images: [
             "learn-english-1.jpg",
             "learn-english-2.jpg",
         ]
+    },
+
+    {
+        titles: [
+            "Do you ever feel lost reading in English?",
+            "Do you ever confused by something like this?",
+            "English can be hard. Do you need any help?"
+        ],
+        "images": [
+            "figure-of-speech-piece-of-cake.jpg",
+            "funny-appendix-removal.jpg",
+            "funny-big-fan-of-yours.jpg",
+            "funny-birds-meet-online.gif",
+            "funny-bullet-got-fired.jpg",
+            "funny-english-is-hard-examples.jpg",
+            "funny-goose-bumps.jpg",
+            "funny-ice-cream-screams.jpg",
+            "funny-kick-the-bucket.jpg",
+            "funny-leaves-fall-for-you.jpg",
+            "funny-problems-and-issues.jpg",
+            "funny-rock-rule.jpg",
+            "funny-screw-screws-up.jpg",
+            "funny-spaghetti-meets-ball.jpg",
+            "funny-squirrel-realizes-hes-nuts.jpg",
+            "funny-sugar-daddy.jpg",
+            "funny-you-drive-crazy-screwdriver.jpg"
+        ]
+    },
+
+    {
+        titles: [
+            "How do I spell this?",
+            "How do I pronounce this?",
+            "What does this mean?",
+            "Words with multiple meanings."
+        ],
+        "images": [
+            "funny-getting-in-the-airplane.jpg",
+            "funny-irony-your-an-idiot.jpg",
+            "funny-lets-eat-grandma.jpg",
+        ]
+
+    },
+
+    {
+        titles: [
+            "Need help with pronunciation?"
+        ],
+        images: [
+            "funny-quote-question-mark.jpg"
+        ]
     }
 
 ];
-
-const slide_texts = [
-    "Don't have dreams. Have goals.",
-    "Learn and Enjoy with Miss & Mister Language",
-    "Consigue tus objetivos en 2 meses con nuestro pack de 35 horas!",
-    "Necesitas ayuda para preparte con tu exámen oficial? Te ayudamos!"
-];
-
-
-
-const slide_image_files = [
-    "learn-catalan-1.jpg",
-    "learn-english-1.jpg",
-    "learn-english-2.jpg",
-    "learn-spanish-1.png",
-    "funny-alligator-in-vest.jpg",
-    "funny-a-piece-of-cake.jpg",
-    "funny-appendix-removal.jpg",
-    "funny-attractive-magnets.png",
-    "funny-bear-deer-fight.jpg",
-    "funny-big-fan-of-yours.jpg",
-    "funny-birds-meet-online.gif",
-    "funny-bullet-got-fired.jpg",
-    "funny-cat-with-glasses-reading-english.jpg",
-    "funny-coffee-and-mug-empty.jpg",
-    "funny-english-is-hard-examples.jpg",
-    "funny-fixed-lamp.jpg",
-    "funny-getting-in-the-airplane.jpg",
-    "funny-goose-bumps.jpg",
-    "funny-homer-simpson-celebrates.jpg",
-    "funny-ice-cream-screams.jpg",
-    "funny-irony-your-an-idiot.jpg",
-    "funny-kick-the-bucket.jpg",
-    "funny-learn-english-to-avoid-mistakes.jpg",
-    "funny-leaves-fall-for-you.jpg",
-    "funny-lets-eat-grandma.jpg",
-    "funny-lost-sock.jpg",
-    "funny-no-words-in-things.jpg",
-    "funny-problems-and-issues.jpg",
-    "funny-quote-question-mark.jpg",
-    "funny-rock-rule.jpg",
-    "funny-screw-screws-up.jpg",
-    "funny-spaghetti-meets-ball.jpg",
-    "funny-squirrel-realizes-hes-nuts.jpg",
-    "funny-sugar-daddy.jpg",
-    "funny-the-fourth-ape.jpg",
-    "funny-weird-english-words-tough-thorough-etc.jpg",
-    "funny-you-drive-crazy-screwdriver.jpg"
-];
-
 
 const google_fonts = [
     "Lobster",
@@ -148,12 +149,15 @@ function set_slideshow_interval() {
 
     return setInterval(function () {
 
-        let selected_title_text = random_choice(slide_texts);
-        while (selected_title_text === last_used_title_text) {
-            selected_title_text = random_choice(slide_texts);
+        const slides_info = random_choice(slides_many_to_many);
+
+        let selected_title_text = random_choice(slides_info.titles);
+        if (slides_info.titles.length > 1) {
+            while (selected_title_text === last_used_title_text) {
+                selected_title_text = random_choice(slide_texts);
+            }
         }
         last_used_title_text = selected_title_text;
-
         slide_title.innerText = selected_title_text;
 
         let selected_google_font = random_choice(google_fonts);
@@ -164,17 +168,17 @@ function set_slideshow_interval() {
         console.log({selected_google_font});
         slide_title.style.fontFamily = selected_google_font;
 
-        let selected_image_file = random_choice(slide_image_files);
-        while (selected_image_file === last_used_image_file) {
-            selected_image_file = random_choice(slide_image_files);
+        let selected_image_file = random_choice(slides_info.images);
+        if (slides_info.images.length > 1) {
+            while (selected_image_file === last_used_image_file) {
+                selected_image_file = random_choice(slide_image_files);
+            }
         }
         last_used_image_file = selected_image_file;
-
         random_image.src = "./slide-images/" + selected_image_file;
 
         const degrees = random_int_from_range(0, 360);
         const color = random_color();
-
         document.body.style.background = `linear-gradient(${degrees}deg, ${color}, transparent)`;
 
     }, INTERVAL_DURATION_IN_SECONDS * 1000);
